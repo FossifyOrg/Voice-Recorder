c919505eddefe638b13fe950b8551a07665ca71b
Naveen <snaveen935@gmail.com>
2024-02-17 22:29:53 +0530

    Change recording notification icon

--- app/src/main/kotlin/org/fossify/voicerecorder/services/RecorderService.kt
+++ app/src/main/kotlin/org/fossify/voicerecorder/services/RecorderService.kt
@@ -258,7 +258,7 @@ class RecorderService : Service() {
         }
 
         var priority = Notification.PRIORITY_DEFAULT
-        var icon = org.fossify.commons.R.drawable.ic_microphone_vector
+        var icon = R.drawable.ic_graphic_eq_vector
         var title = label
         var visibility = NotificationCompat.VISIBILITY_PUBLIC
         var text = getString(R.string.recording)
