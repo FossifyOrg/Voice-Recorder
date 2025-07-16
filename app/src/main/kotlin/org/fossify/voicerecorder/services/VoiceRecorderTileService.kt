bd53df0aa59000cbf9945bae80d69d02dbe7d852
Gerald Schneider <gerald@schneidr.de>
2025-07-16 20:32:36 +0200

    added TileService

---
+++ app/src/main/kotlin/org/fossify/voicerecorder/services/VoiceRecorderTileService.kt
@@ -0,0 +1,39 @@
+package org.fossify.voicerecorder.services
+
+import android.content.Intent
+import android.service.quicksettings.Tile
+import android.service.quicksettings.TileService 
+import org.fossify.commons.extensions.toast
+import org.fossify.voicerecorder.R
+import org.fossify.voicerecorder.services.RecorderService
+
+class VoiceRecorderTileService: TileService() {
+  // Called when the user taps on your tile in an active or inactive state.
+  override fun onClick() {
+    super.onClick()
+    Intent(this@VoiceRecorderTileService, RecorderService::class.java).apply {
+        // try {
+            if (qsTile.state == Tile.STATE_INACTIVE) {
+              // RecorderService.startRecording()
+              if (!RecorderService.isRunning) {
+                  startService(this)
+              }
+              toast("Start")
+              qsTile.state = Tile.STATE_ACTIVE
+              qsTile.label = "stop recording"
+            }
+            else {
+              toast("Stop")
+              // RecorderService.stopRecording()
+              if (RecorderService.isRunning) {
+                  stopService(this)
+              }
+              qsTile.state = Tile.STATE_INACTIVE
+              qsTile.label = "start recording"
+            }
+            qsTile.updateTile()
+    //     } catch (ignored: Exception) {
+    //     }
+    }
+  }
+}
