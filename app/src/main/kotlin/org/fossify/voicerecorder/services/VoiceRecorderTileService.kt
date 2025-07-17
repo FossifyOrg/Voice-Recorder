2e75b1bc9569edd49030e604b2ffb5cbb938f2e0
Gerald Schneider <gerald@schneidr.de>
2025-07-17 19:11:06 +0200

    Added EventBus notification

--- app/src/main/kotlin/org/fossify/voicerecorder/services/VoiceRecorderTileService.kt
+++ app/src/main/kotlin/org/fossify/voicerecorder/services/VoiceRecorderTileService.kt
@@ -3,31 +3,39 @@ package org.fossify.voicerecorder.services
 import android.content.Intent
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService 
+import org.fossify.voicerecorder.helpers.RECORDING_RUNNING
+import org.fossify.voicerecorder.helpers.RECORDING_STOPPED
+import org.fossify.voicerecorder.models.Events
+import org.fossify.voicerecorder.R
 import org.fossify.voicerecorder.services.RecorderService
+import org.greenrobot.eventbus.EventBus
 
 class VoiceRecorderTileService: TileService() {
+  private var status = RECORDING_STOPPED
+
   // Called when the user taps on your tile in an active or inactive state.
   override fun onClick() {
     super.onClick()
     Intent(this@VoiceRecorderTileService, RecorderService::class.java).apply {
         // try {
-            if (qsTile.state == Tile.STATE_INACTIVE) {
-              // RecorderService.startRecording()
+            if (status == RECORDING_STOPPED) {
               if (!RecorderService.isRunning) {
                   startService(this)
               }
               qsTile.state = Tile.STATE_ACTIVE
               qsTile.label = "stop recording"
+              status = RECORDING_RUNNING
             }
             else {
-              // RecorderService.stopRecording()
               if (RecorderService.isRunning) {
                   stopService(this)
               }
               qsTile.state = Tile.STATE_INACTIVE
-              qsTile.label = "start recording"
+              qsTile.label = getString(R.string.recording)
+              status = RECORDING_STOPPED
             }
             qsTile.updateTile()
+            EventBus.getDefault().post(Events.RecordingStatus(status))
     //     } catch (ignored: Exception) {
     //     }
     }
