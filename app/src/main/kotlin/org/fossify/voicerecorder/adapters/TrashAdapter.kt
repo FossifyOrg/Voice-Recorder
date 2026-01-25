34ae32dd38de70f2058b10449fa222e2106f2521
Naveen Singh <36371707+naveensingh@users.noreply.github.com>
2026-01-26 00:08:41 +0530

    feat: add font customization (#293)
    * fix: use AndroidX AppCompat PopupMenu
    
    * feat: add font customization

--- app/src/main/kotlin/org/fossify/voicerecorder/adapters/TrashAdapter.kt
+++ app/src/main/kotlin/org/fossify/voicerecorder/adapters/TrashAdapter.kt
@@ -6,7 +6,7 @@ import android.view.Gravity
 import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
-import android.widget.PopupMenu
+import androidx.appcompat.widget.PopupMenu
 import com.qtalk.recyclerviewfastscroller.RecyclerViewFastScroller
 import org.fossify.commons.adapters.MyRecyclerViewAdapter
 import org.fossify.commons.dialogs.ConfirmationDialog
